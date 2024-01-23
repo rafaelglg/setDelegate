4e0da4dbefaa5d5acc09c7bef610563991f038cf
rafaelglg <rafaelglg9@gmail.com>
2024-01-23 13:15:56 +0100

    add test unitarios

--- setDelegateTests/setDelegateTests.swift
+++ setDelegateTests/setDelegateTests.swift
@@ -6,30 +6,30 @@
 //
 
 import XCTest
+import PDFKit
+
+@testable import setDelegate
 
 final class setDelegateTests: XCTestCase {
+    
+    var pdfViewTest: PDFView?
+
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        pdfViewTest = PDFView()
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        pdfViewTest = nil
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
-        }
+    func testPDFViewIsNotNil() {
+        
+        let pdfViewController = PDFViewController()
+        pdfViewController.pdfView = pdfViewTest
+        pdfViewController.loadViewIfNeeded()
+        
+        XCTAssertNotNil(pdfViewTest, "PDFView should not be nil after loading the view")
     }
 
 }
